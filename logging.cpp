30ab8352cd34255658ae0c88df6af702675f8d3a
Loater69 <kaiserluis@gmx.de>
2023-03-01 20:34:03 +0100

    fixed bug

--- logging.cpp
+++ logging.cpp
@@ -1,6 +1,6 @@
 #include "logging.h"
 
-logging::logger::logger(const std::string& log_file) : logging_file(log_file, std::ios::ate), std::basic_ostream<char>(logging_file.rdbuf()) {}
+logging::logger::logger(const std::string& log_file) : logging_file(log_file, std::ios::app), std::basic_ostream<char>(logging_file.rdbuf()) {}
 
 logging::logger::~logger() {
     logging_file.close();
